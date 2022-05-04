376ce6c4a63da28ed470a51903ce7ffa58c7bd53
NatalyaQA <101937723+NatalyaQA@users.noreply.github.com>
2022-05-04 20:49:18 +0500

    Ready

---
+++ lesson2.1_step5.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+import time
+import math
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+  
+try: 
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+        
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+ 
+    browser.find_element_by_id("answer").send_keys(y)
+
+    option1 = browser.find_element_by_css_selector("[for='robotCheckbox']")
+    option1.click()
+    
+    option2 = browser.find_element_by_css_selector("[for='robotsRule']")
+    option2.click()
+    
+   
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+    browser.find_element_by_xpath('//*[@type="submit"]').click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+    
+        
+    #python lesson2.1_step5.py
